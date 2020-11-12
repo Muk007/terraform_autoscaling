948c096b2b0e774eb18f2d5bd702f80f6f457689
mukesh <Mukesh>
2020-11-12 09:56:00 +0530

    alarm added

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_metric_alarm"  "metric_1" {
 	alarm_name = "example-cpu-alarm"
+	alarm_description = "example-cpu-alarm"
 	comparison_operator = "GreaterThanOrEqualToThreshold"
 	evaluation_periods = "2"
 	metric_name = "CPUUtilization"
@@ -7,20 +8,31 @@ resource "aws_cloudwatch_metric_alarm"  "metric_1" {
 	period = "120"
 	statistic = "Average"
 	threshold = "30"
-	alarm_description = "example-cpu-alarm"
-	
+
+	dimensions = {
+		"AutoScalingGroupName" = "${aws_autoscaling_group.asg_example.name}"
+	}
+
+	actions_enabled = true
+	alarm_actions  = ["${aws_autoscaling_policy.cpu_policy_1.arn}"]
 }
 
 resource "aws_cloudwatch_metric_alarm"  "metric_2" {
         alarm_name = "example-cpu-alarm"
-        comparison_operator = "LessThanThreshold"
+        alarm_description = "example-cpu-alarm"
+	comparison_operator = "LessThanThreshold"
         evaluation_periods = "2"
         metric_name = "CPUUtilization"
         namespace = "AWS/EC2"
         period = "120"
         statistic = "Average"
         threshold = "20"
-        alarm_description = "example-cpu-alarm"
 
+
+	dimensions = {
+        	"AutoScalingGroupName" = "${aws_autoscaling_group.asg_example.name}"
+	}
+
+	actions_enabled = true
+	alarm_actions  = ["${aws_autoscaling_policy.cpu_policy_2.arn}"]
 }
-
